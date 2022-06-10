bcceabf35c2c672d2a32c0a101311ef1c715bb2e
Matias Chomicki <matyax@gmail.com>
2022-06-10 19:13:05 +0200

    Add basic styles for the content editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { FormEvent, useRef, useState } from "react";
+import "./editor.css";
 
 const Editor = () => {
     const [text, setText] = useState("Default text");
+    const editorRef = useRef<HTMLDivElement | null>(null);
 
-    return <div contentEditable>{text}</div>
+    return (
+        <div className="editor-container">
+            <div contentEditable className="editor" ref={editorRef} dangerouslySetInnerHTML={{ __html: text }} />
+        </div>
+    )
 }
 
 export default Editor;
